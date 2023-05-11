1f021aa04a0a17ffd1bce25423934b4ccb4dc485
tiffanyteaze <tteaze@gmail.com>
2023-05-10 23:27:43 -0700

    closes divs

--- closeDivs.js
+++ closeDivs.js
@@ -12,7 +12,34 @@ let htmlString = `<div>
   <div>`;
 
 const closeSecondDiv = (string) => {
-    console.log(string)
+    let divCounter = 0;
+    let unknownFour = "";
+    let fixedHTML = "";
+    
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "<") {
+            for (let j = 1; j < 5; j++) {
+                unknownFour += string[i + j];
+            }
+            // console.log(unknownFour);
+        }
+
+        if (unknownFour === "div>") {
+            divCounter++;
+            if (divCounter % 2 === 0) {
+                fixedHTML += string[i] +"/";
+                unknownFour = "";
+                continue;
+            }
+        }
+    
+        fixedHTML += string[i];
+        unknownFour = "";
+    }
+    return fixedHTML;
 }
 
-closeSecondDiv("hello");
+closeSecondDiv(htmlString);
+
+console.log(closeSecondDiv("<div><p>Here is a <div> tag</p>"));
+console.log(closeSecondDiv(htmlString));
