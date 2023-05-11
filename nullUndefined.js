0fd92de07f10675f9a0fe4e57eb14d5edd727c03
tiffanyteaze <tteaze@gmail.com>
2023-05-11 00:21:39 -0700

    adds context between null and undefined

---
+++ nullUndefined.js
@@ -0,0 +1,15 @@
+//What will be logged in the first example and in second example?
+
+let var1;
+console.log(var1);
+console.log(typeof var1);
+
+let var2 = null;
+console.log(var2);
+console.log(typeof var2)
+
+/* Undefined means the variable has been declared, but its value 
+has not been assigned. 
+
+Null means an empty value or a blank value. It's assigned as a
+representation of no value. */
