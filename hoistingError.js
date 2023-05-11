804af9407447080a84752c518be571ef6a345d66
tiffanyteaze <tteaze@gmail.com>
2023-05-11 00:20:01 -0700

    adds hoisting error question

---
+++ hoistingError.js
@@ -0,0 +1,16 @@
+// What will be console logged here?
+
+//Question 1
+// console.log(foo);
+// foo = 1;
+
+//Question 2
+// console.log(foo);
+// var foo = 2;
+
+//Question 3
+foo = 3;
+console.log(foo);
+var foo;
+
+/* const and let don't bubble like var */
